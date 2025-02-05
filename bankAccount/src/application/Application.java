e027ef4dbd34b180e0197a0bc04257689220d2fb
Gabriel Lisboa <gabriel.lisboa.ti@gmail.com>
2025-02-04 21:12:00 -0300

    All account classes createds

--- bankAccount/src/application/Application.java
+++ bankAccount/src/application/Application.java
@@ -1,9 +1,20 @@
 package application;
 
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
 public class Application {
 	
 	public static void main(String[] args) {
 		
+		Account baseAccount = new Account("Gabriel", 20.000);
+		BusinessAccount ba = new BusinessAccount("Gabriel", 20.000, 1.000);
+		SavingsAccount sa = new SavingsAccount(baseAccount.getHolder(), 5.000, 1.5);
+		
+		System.out.println(baseAccount.getAccountNumber());
+		System.out.println(ba.getAccountNumber());
+		System.out.println(sa.getAccountNumber());
 	}
 
 }
